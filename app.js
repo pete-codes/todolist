b18a5f5f7855b33b574eb154d2beabc713f478aa
Pete Codes <38405508+pete-codes@users.noreply.github.com>
2019-10-17 16:35:39 +0100

    3rd commit
    Added style sheet
    Can now log todos

--- app.js
+++ app.js
@@ -9,25 +9,78 @@ const bodyParser = require("body-parser");
 
 
 const app = express();
+var items = [];
+// var idea="";
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 
 app.get("/", function(req, res){
+
+  // if ("day" === 0 || 6) {
+  //   res.write("this is a weekend");
+  // }
+  //
+  // else {
+  //   res.write("this is a weekday");
+  // }
+
+
 var today = new Date();
-  var currentDay = today.getDay();
-var day = "";
+var options = {
+  weekday:"long",
+  day:"numeric",
+  month:"long",
+  year:"numeric"
+};
 
-  if (currentDay === 5 || currentDay === 0) {
-    day= "weekend";
-  }
+var day= today.toLocaleDateString("en-GB", options);
 
-  else {
-    day= "weekday";
 
-  }
-res.render("list", {kindOfDay:day});
+// var currentDay = today.getDay();
+
+
+
+
+
+//   switch (currentDay){
+//
+//   case 0: day= "Sunday";
+//
+// break;
+//
+//   case 1: day="Monday";
+//
+// break;
+//     case 2:day="Tuesday";
+//
+// break;
+//     case  3: day="Wednesday";
+//
+// break;
+//   case  4:day="Thursday";
+//
+// break;
+//     case  5: day="Friday";
+//
+// break;
+//     case  6: day="Saturday";
+//
+//       }
+
+res.render("list", {kindOfDay:day, newListItem:items});
 });
+
+app.post("/", function (req,res){
+   var item = req.body.newItem;
+   items.push(item);
+// });
+
+res.redirect("/");
+// res.ren  der("list", {newListItem:item});
+ });
 
 
 
